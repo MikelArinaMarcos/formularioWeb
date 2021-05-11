e9280e017955e0b972e9e3ccbc4081845a62a4b0
MikelArinaMarcos <mikel.arina@estudiantat.upc.edu>
2021-05-11 07:24:21 +0200

    Validación creada

---
+++ validacion.js
@@ -0,0 +1,13 @@
+function validar(){
+    var nombre, apellido, email, confemail, dni, movil, expresion;
+    nombre = document.getElementById("nombre").value;
+    apellido = document.getElementById("apellido").value;
+    email = document.getElementById("email").value;
+    confemail = document.getElementById("confemail").value;
+    dni = document.getElementById("dni").value;
+    movil = document.getElementById("movil").value;
+
+    if(nombre == ""){
+        alert("Introduzca el nombre");
+    }
+}
